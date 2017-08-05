0b6151401dab73a1013045eaafe62a9519464e94
Jake Helbig <jake@jakehelbig.com>
2017-08-05 00:18:04 -0500

    initial completion from font-awesome plugin to font-aws-ome plugin

--- lib/font-aws-ome.component.ts
+++ lib/font-aws-ome.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
+import aws_color from './dict/aws_color';
+import aws_color_flat from './dict/aws_color_flat';
+import aws_grey from './dict/aws_grey';
+import aws_grey_flat from './dict/aws_grey_flat';
 
 @Component({
   selector: 'faws, ng2-faws, ng4-faws, ng-faws',
-  template: `
-    <span class="faws-{{name}}" [ngClass]="optionalClasses" aria-hidden="true">{{parts}}</span>
-  `
+  templateUrl: './font-aws-ome.component.html',
+  styleUrls: ['./styles/aws-ome.min.css']
 })
 export class FontAwsOmeComponent implements OnInit {
   @Input() name        : string;
   // Optional Inputs
   @Input() title      ?: string;
   @Input() size       ?: string;
-  //@Input() fixed      ?: boolean;
-  //@Input() animation  ?: string;
-  //@Input() rotate     ?: string | number;
-  //@Input() inverse    ?: boolean;
-  @Input() color      ?: boolean;
-  @Input() flat       ?: boolean;
+  @Input() animation  ?: string;
+  @Input() rotate     ?: string | number;
+  @Input() invert    ?: string = "false";
+  @Input() color      ?: string = "true";
+  @Input() flat       ?: string = "false";
 
   private _optionalClasses: string[] = [];
-  private _parts:           string[] = [];
+  public children:           any[] = [];
   constructor() { }
 
   ngOnInit() {
@@ -31,33 +33,49 @@ export class FontAwsOmeComponent implements OnInit {
       this.addToOptionalClasses(`faws-${this.size}`);
     }
     
-    if(this.color) {
-      this.name "color-" + this.name;
-    }else{
-      this.name "grey-" + this.name;
+    // 3D color
+    if (this.color == "true" && this.flat == "false") {
+      this.children = aws_color.icons.filter(icon => { return icon.properties.name == this.name; });
+      this.name = "color-" + this.name;
     }
     
-    if(this.flat) {
-      this.name "flat-" + this.name;
+    // Flat color
+    if (this.color == "true" && this.flat == "true") {
+      this.children = aws_color_flat.icons.filter(icon => { return icon.properties.name == this.name; });
+      this.name = "color-flat-" + this.name;
+    }
+    
+    // 3D Grey
+    if (this.color == "false" && this.flat == "false") {
+      this.children = aws_grey.icons.filter(icon => { return icon.properties.name == this.name; });
+      this.name = "grey-" + this.name;
+    }
+    
+    // Flat grey
+    if (this.color == "false" && this.flat == "true") {
+      this.children = aws_grey_flat.icons.filter(icon => { return icon.properties.name == this.name; });
+      this.name = "grey-flat-" + this.name;
+    }
+    
+    if (this.children.length == 0) {
+      throw new Error('Unknown "name" property for Font AWS-ome component');
+    }else{
+      this.children = this.children[0].attrs;
     }
 
-    //if(this.fixed) {
-    //  this.addToOptionalClasses(`faws-fw`);
-    //}
+    if(this.animation) {
+      this.addToOptionalClasses(`faws-${this.animation}`);
+    }
 
-    //if(this.animation) {
-    //  this.addToOptionalClasses(`faws-${this.animation}`);
-    //}
+    if(this.rotate){
+      let rotateClass = (typeof this.rotate === 'number') ? `faws-rotate-${this.rotate}`
+                                                          : `faws-flip-${this.rotate}`;
+      this.addToOptionalClasses(rotateClass);
+    }
 
-    //if(this.rotate){
-    //  let rotateClass = (typeof this.rotate === 'number') ? `faws-rotate-${this.rotate}`
-    //                                                      : `faws-flip-${this.rotate}`;
-    //  this.addToOptionalClasses(rotateClass);
-    //}
-
-    //if(this.inverse) {
-    //  this.addToOptionalClasses(`fa-inverse`);
-    //}
+    if(this.invert == "true") {
+      this.addToOptionalClasses(`faws-inverse`);
+    }
 
   }
 
